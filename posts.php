8db2af8dac543540c2a28555adaecbd4145fd092
Michael Dionne <dionnemj1@gmail.com>
2014-03-29 17:43:59 -0700

    added post bootstrap

--- posts.php
+++ posts.php
@@ -1,25 +1,35 @@
 <?php theme_include('header'); ?>
 
-<section class="sheet answer">
-<!-- 	<header class="ribbon"><?php echo page_title(); ?></header> -->
-	<?php if(has_posts()): ?>
-		<!-- We have posts, it's safe to loop. -->
-		<?php while(posts()): ?>
-		     <h1><?php echo article_title(); ?></h1>
-		     <?php echo article_markdown(); ?>
-		<?php endwhile; ?>
 
-		<?php if(has_pagination()): ?>
-		<nav class="pagination">
-			<div class="wrap">
-				<?php echo posts_prev(); ?>
-				<?php echo posts_next(); ?>
-			</div>
-		</nav>
+	<div class="content-header">
+      <h1>
+          <a id="menu-toggle" href="#" class="btn btn-default"><i class="icon-reorder"></i></a>
+          Simple Sidebar
+      </h1>
+  </div>
+
+<div class="page-content inset">
+	<section class="sheet answer">
+	<!-- 	<header class="ribbon"><?php echo page_title(); ?></header> -->
+		<?php if(has_posts()): ?>
+			<!-- We have posts, it's safe to loop. -->
+			<?php while(posts()): ?>
+			     <h1><?php echo article_title(); ?></h1>
+			     <?php echo article_markdown(); ?>
+			<?php endwhile; ?>
+
+			<?php if(has_pagination()): ?>
+			<nav class="pagination">
+				<div class="wrap">
+					<?php echo posts_prev(); ?>
+					<?php echo posts_next(); ?>
+				</div>
+			</nav>
+			<?php endif; ?>
+		<?php else: ?>
+			<p>Looks like you have some writing to do!</p>
 		<?php endif; ?>
-	<?php else: ?>
-		<p>Looks like you have some writing to do!</p>
-	<?php endif; ?>
-</section>
+	</section>
+</div>
 
 <?php theme_include('footer'); ?>
