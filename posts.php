178131105254df4ee09249775c8f81758a0a7725
Michael Dionne <dionnemj1@gmail.com>
2014-03-31 18:13:30 -0700

    Edited footer CSS and got ride of author tag

--- posts.php
+++ posts.php
@@ -1,14 +1,13 @@
 <?php theme_include('partials/header'); ?>
 
-<!-- +++++ Posts Lists +++++ -->	
-<?php if(has_posts()): ?>    
+<!-- +++++ Posts Lists +++++ -->
+<?php if(has_posts()): ?>
 <!-- We have posts, it's safe to loop. -->
     <?php $i = 0; while(posts()): $i++; ?>
-	<div id="bg<?php echo $i % 2; ?>"> 
+	<div id="bg<?php echo $i % 2; ?>">
 	    <div class="container">
 			<div class="row">
-				<div class="col-lg-8 col-lg-offset-2">                
-                    <p><img src="<?php echo theme_url('assets/img/user.png'); ?>" width="50px" height="50px"> <ba><?php echo article_author(); ?></ba></p>
+				<div class="col-lg-8 col-lg-offset-2">
                     <p><bd><?php echo article_date(); ?></bd></p>
                     <h4><?php echo article_title(); ?></h4>
                     <p><?php echo article_description(); ?></p>
@@ -19,7 +18,7 @@
      </div><!-- /#bg -->
     <?php endwhile; ?>
 
-<?php if(has_pagination()): ?>           
+<?php if(has_pagination()): ?>
     <div id="grey">
         <div class="col-lg-8 col-lg-offset-2">
         <?php echo posts_prev('# Back'); ?>
@@ -27,14 +26,14 @@
         </div>
     </div><!-- /#grey -->
 <?php endif; ?>
-                
+
 <?php else: ?>
     <div id="white">
         <div class="col-lg-8 col-lg-offset-2">
             <p>Looks like you have some writing to do!</p>
         </div>
     </div><!-- /#white -->
-<?php endif; ?>     
-<!-- +++++ Posts Lists +++++ -->	
+<?php endif; ?>
+<!-- +++++ Posts Lists +++++ -->
 
 <?php theme_include('partials/footer'); ?>
