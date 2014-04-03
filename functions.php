e74a354567e32f5b9ac022dec219813ba44ba67d
michaeldionne <dionnemj1@gmail.com>
2014-04-03 15:46:11 -0700

    Added estimated article reading time function

--- functions.php
+++ functions.php
@@ -101,3 +101,21 @@ function rwar_latest_posts($limit = 6) {
 
 	return $result;
 }
+
+// Estimated article reading time
+function dg_est_reading_time() {
+	$mycontent = article_html();
+	$word = str_word_count(strip_tags($mycontent));
+	$m = floor($word / 175);
+	$s = floor($word % 175 / (175 / 60));
+
+	if($m >= 1) {
+
+	return $est = $m . ' min' . ($m == 1 ? '' : 's');
+	}
+
+	elseif($s <= 59) {
+
+	return $est = $s . ' second' . ($s == 1 ? '' : 's');
+	}
+}
