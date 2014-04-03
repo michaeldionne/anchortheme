310581d2b0abec81bdb342321e62420775dc3580
michaeldionne <dionnemj1@gmail.com>
2014-04-03 16:11:45 -0700

    Cleaned up markup

--- posts.php
+++ posts.php
@@ -8,7 +8,7 @@
 	    <div class="container">
 			<div class="row">
 				<div class="col-lg-10 col-lg-offset-1">
-                    			<div class="col-lg-4">
+                    <div class="col-lg-4">
                     	<h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
                     	<p><bd><?php echo article_date(); ?>    |    <?php echo dg_est_reading_time(); ?></bd></p>
                     </div>
