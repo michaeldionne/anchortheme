3d9f33d9974c9970d8deeeb49fcd93870ab71776
michaeldionne <dionnemj1@gmail.com>
2014-04-03 16:06:31 -0700

    markup changes to revert

--- posts.php
+++ posts.php
@@ -9,10 +9,9 @@
 			<div class="row">
 				<div class="col-lg-10 col-lg-offset-1">
                     			<div class="col-lg-4">
-                    				<h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
-                    				<p><bd><?php echo article_date(); ?>    |    <?php echo dg_est_reading_time(); ?></bd></p>
-                    			</div>
-                    		</div>
+                    	<h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
+                    	<p><bd><?php echo article_date(); ?>    |    <?php echo dg_est_reading_time(); ?></bd></p>
+                    </div>
                     <div class="col-lg-8">
                     	<p><?php echo article_markdown(); ?></p>
                     </div>
