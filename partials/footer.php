364a97e4991815f5ad774bbce8c40ae9d764606b
michaeldionne <dionnemj1@gmail.com>
2014-04-07 14:49:57 -0700

    Added google analytics script

--- partials/footer.php
+++ partials/footer.php
@@ -12,5 +12,17 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="<?php echo theme_url('assets/js/bootstrap.min.js'); ?>"></script>
+    
+    <!-- Google Analytics Script --!>
+    
+    	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-39195978-2', 'michaeldionne.co');
+	  ga('send', 'pageview');
+	</script>
 </body>
 </html>
