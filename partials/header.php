7309e8c01abb01e83950d88d3ed0709671a02074
michaeldionne <dionnemj1@gmail.com>
2014-05-14 12:16:38 -0700

    Changed Google Analytics

--- partials/header.php
+++ partials/header.php
@@ -8,17 +8,8 @@
     <meta name="description" content="<?php echo site_description(); ?>">
     <title><?php echo site_name(); ?></title>
 
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <?php echo site_meta('google_analytics'); ?>
 	
-	  ga('create', 'UA-39195978-2', 'michaeldionne.co');
-	  ga('send', 'pageview');
-	
-	</script>
-
      <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="<?php echo theme_url('assets/css/bootstrap.css'); ?>">
     <!-- Custom styles for this template -->
