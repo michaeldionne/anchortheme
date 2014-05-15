76f2b29b8d0aeda6cb2a543c4561dec3e4b898aa
michaeldionne <dionnemj1@gmail.com>
2014-05-15 09:45:37 -0700

    Attempting to add custom page css field to page

--- page.php
+++ page.php
@@ -1,5 +1,7 @@
 <?php theme_include('partials/header'); ?>
 
+<li style=".navbar-inverse {background-color:<?php echo page_custom_field('custom_header_color'); ?>}">
+
     <div class="container">
         <div class="row mt resp centered">
             <h1><?php echo page_title(); ?></h1>
