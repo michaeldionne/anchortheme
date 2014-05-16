61b0dc270c779e0114b3db9ae2a0d6b6592eddf4
michaeldionne <dionnemj1@gmail.com>
2014-05-16 10:08:45 -0700

    Vertical centering changes for footer

--- partials/footer.php
+++ partials/footer.php
@@ -3,8 +3,8 @@
 	<div id="footer">
 		<div class="container">
 			<div class="row" >
-				<p class="pull-left">&copy;2014 Michael Dionne</p>
-				<p class="pull-right">If you want to follow me, you can reach me here: <a href="https://twitter.com/itsmikedionne" title="Twitter">Twitter</a> | <a href="http://instagram.com/mr_dionne" title="Instagram">Instagram</a>  | <a href="http://michaeldionne.vsco.co" title="VSCO">VSCO</a> | <a href="mailto:me@michaeldionne.co" title="Email">Email</a></p>
+				<p class="pull-left" style="height:100px; margin-top:-50px; position:absolute;top:50%" >&copy;2014 Michael Dionne</p>
+				<p class="pull-right" style="height:100px; margin-top:-50px; position:absolute;top:50%" >If you want to follow me, you can reach me here: <a href="https://twitter.com/itsmikedionne" title="Twitter">Twitter</a> | <a href="http://instagram.com/mr_dionne" title="Instagram">Instagram</a>  | <a href="http://michaeldionne.vsco.co" title="VSCO">VSCO</a> | <a href="mailto:me@michaeldionne.co" title="Email">Email</a></p>
 			</div>
 			<!-- /row -->
 
