4366e16dfe45d1c1c7fbcfec17fb37d57e6edc71
michaeldionne <dionnemj1@gmail.com>
2014-05-16 10:13:27 -0700

    Making footer text responsive

--- partials/footer.php
+++ partials/footer.php
@@ -3,8 +3,8 @@
 	<div id="footer">
 		<div class="container">
 			<div class="row" >
-				<p class="pull-left">&copy;2014 Michael Dionne</p>
-				<p class="pull-right">If you want to follow me, you can reach me here: <a href="https://twitter.com/itsmikedionne" title="Twitter">Twitter</a> | <a href="http://instagram.com/mr_dionne" title="Instagram">Instagram</a>  | <a href="http://michaeldionne.vsco.co" title="VSCO">VSCO</a> | <a href="mailto:me@michaeldionne.co" title="Email">Email</a></p>
+				<p class="col-md-4">&copy;2014 Michael Dionne</p>
+				<p class=".col-md-4 .col-md-offset-4">If you want to follow me, you can reach me here: <a href="https://twitter.com/itsmikedionne" title="Twitter">Twitter</a> | <a href="http://instagram.com/mr_dionne" title="Instagram">Instagram</a>  | <a href="http://michaeldionne.vsco.co" title="VSCO">VSCO</a> | <a href="mailto:me@michaeldionne.co" title="Email">Email</a></p>
 			</div>
 			<!-- /row -->
 
