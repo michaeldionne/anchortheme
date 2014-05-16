862549e0f3b9874700735b63cd8655af8e61c0fd
michaeldionne <dionnemj1@gmail.com>
2014-05-16 10:20:37 -0700

    Removed page title from php

--- page.php
+++ page.php
@@ -4,7 +4,6 @@
 
     <div class="container">
         <div class="row mt resp centered">
-            <h1><?php echo page_title(); ?></h1>
             <p><?php echo page_content(); ?></p>
         </div><!-- /row -->
     </div><!-- /container -->
