6f32d3963c9daa148dc0bee595d1788f92432e4d
Michael <dionnemj1@gmail.com>
2014-05-17 23:20:10 -0700

    Moved share buttons

--- article.php
+++ article.php
@@ -8,7 +8,6 @@
             	<div class="col-lg-4">
                     <h1><a href="<?php echo article_url(); ?>"><?php echo article_title(); ?></a></h1>
                     <p><bd><?php echo article_date(); ?>    |    <?php echo dg_est_reading_time(); ?></bd></p>
-                    <?php echo site_meta('share_buttons'); ?>
                 </div>
                 <div class="col-lg-8">
                     <p><?php echo article_markdown(); ?></p>
@@ -18,7 +17,7 @@
 		<div class="row mt">
 			<div class="col-lg-8 col-lg-offset-2">
 				<hr>
-				<p><bt>Category: <a href="<?php echo category_url(); ?>"><?php echo category_title(); ?></a></bt> - <bt>Written: <a><?php echo article_date(); ?></a></bt><?php echo site_meta('share-buttons-mobile'); ?></p>
+				<p><bt>Category: <a href="<?php echo category_url(); ?>"><?php echo category_title(); ?></a></bt> - <bt>Written: <a><?php echo article_date(); ?></a></bt><?php echo site_meta('share_buttons'); ?></p>
 					<p><a href="http://michaeldionne.co" onclick="goBack()">Back to Main</a></p>
 			</div>
 		</div><!-- /row -->
